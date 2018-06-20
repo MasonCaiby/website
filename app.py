982946558d96c481d00c92fa327d6844f62b8ea5
MasonCaiby <maxwell.caudle@gmail.com>
2018-06-20 11:33:10 -0600

    pep8

--- app.py
+++ app.py
@@ -1,55 +1,64 @@
 from flask import Flask, request, session, g, url_for, \
-render_template, flash
+    render_template, flash
 import pigpio
 import numpy as np
 import os
 import time
 from helpers import fade_colors
 
+
 app = Flask(__name__)
 app.debug = True
 
 app.config.from_envvar('LIGHT_CONTROLS_SETTINGS', silent=True)
+
 
 def startup_pigpio():
     global pi1
     os.system('sudo /home/pi/website/startup_commands.sh')
     time.sleep(1)
     pi1 = pigpio.pi()
-    pi1.set_PWM_dutycycle(17,0)
-    pi1.set_PWM_dutycycle(27,0)
-    pi1.set_PWM_dutycycle(22,0)
+    pi1.set_PWM_dutycycle(17, 0)
+    pi1.set_PWM_dutycycle(27, 0)
+    pi1.set_PWM_dutycycle(22, 0)
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
+
 @app.route('/climbing')
 def climbing():
     return render_template('climbing.html')
+
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/wildlife')
 def wildlife():
     return render_template('wildlife.html')
+
 
 @app.route('/handler')
 def handler():
     return render_template('handler.html')
 
+
 @app.route('/table')
 def table():
     return render_template('table.html')
 
-@app.route('/controls', methods=['GET','POST'])
+
+@app.route('/controls', methods=['GET', 'POST'])
 def light_controls():
     # safely try to get the current values of the GPIO Pins. If multiple people
     # are controlling the table, it pulls the current value every time they
     # load it. I could move this into the html so people get live updates
-    #TODO: move into html for live update.
+    # TODO: move into html for live update.
 
     try:
         r = pi1.get_PWM_dutycycle(17)
@@ -64,16 +73,19 @@ def light_controls():
 
     if request.method == 'POST':
         try:
-            new_colors = np.array([request.form['red'],request.form['green'],request.form['blue']])
+            new_colors = np.array([request.form['red'], request.form['green'],
+                                  request.form['blue']])
             new_colors = new_colors.astype('int')
-            assert (np.all(new_colors<256) and np.all(new_colors>=0))
+            assert (np.all(new_colors < 256) and np.all(new_colors >= 0))
             fade_colors(pi1, np.array([r, g, b]), new_colors, [17, 27, 22])
             r = pi1.get_PWM_dutycycle(17)
             g = pi1.get_PWM_dutycycle(27)
             b = pi1.get_PWM_dutycycle(22)
-        except (ValueError,AssertionError):
+        except (ValueError, AssertionError):
             flash('You need to type a value between 0 and 255 for all boxes')
-    return render_template('light_controls.html', r_value=r, g_value=g, b_value=b)
+    return render_template('light_controls.html', r_value=r, g_value=g,
+                            b_value=b)
+
 
 if __name__ == '__main__':
     startup_pigpio()
--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
-
 import numpy as np
 import time
+
 
 def convert_colors(image_path, new_value, verbose=False, save_file=False):
 
@@ -12,12 +12,13 @@ def convert_colors(image_path, new_value, verbose=False, save_file=False):
     for i, row in enumerate(arr):
         for j, pixel in enumerate(row):
             if pixel[3] > 0:
-                arr[i,j] = new_value
+                arr[i, j] = new_value
 
     if verbose:
         plt.imshow(arr, interpolation='nearest')
     if save_file:
         misc.imsave(save_file, arr)
+
 
 def fade_colors(pi1, old_colors, new_colors, rgb_gpio):
     deltas = new_colors - old_colors
@@ -25,16 +26,18 @@ def fade_colors(pi1, old_colors, new_colors, rgb_gpio):
     increments = deltas / max_d
     for i in range(max_d):
         old_colors = old_colors + increments
-        #turn negatives into 0
+        # turn negatives into 0
         old_colors = (old_colors + np.absolute(old_colors))/2
         update_strip(pi1, old_colors, rgb_gpio)
         time.sleep(.01)
 
+
 def update_strip(pi1, new_colors, rgb_gpio):
-    pi1.set_PWM_dutycycle(rgb_gpio[0],new_colors[0])
-    pi1.set_PWM_dutycycle(rgb_gpio[1],new_colors[1])
-    pi1.set_PWM_dutycycle(rgb_gpio[2],new_colors[2])
+    pi1.set_PWM_dutycycle(rgb_gpio[0], new_colors[0])
+    pi1.set_PWM_dutycycle(rgb_gpio[1], new_colors[1])
+    pi1.set_PWM_dutycycle(rgb_gpio[2], new_colors[2])
 
 if __name__ == '__main__':
 
-    convert_colors('static/new_window_old.png', [34, 228, 144, 255], save_file='static/new_window.png')
+    convert_colors('static/new_window_old.png', [34, 228, 144, 255],
+                   save_file='static/new_window.png')
